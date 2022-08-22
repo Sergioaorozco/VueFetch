2bf9107cc72426026c0c8457024587db6a7dd828
Sergio <sergioa.orozcoo@gmail.com>
2022-08-22 17:42:33 -0500

    Add spinner when looking for info

--- src/components/BookResult.vue
+++ src/components/BookResult.vue
@@ -2,17 +2,18 @@
   export default {
     data(){
       return{
+        booksLoading: false
       }
     },
     props: ['booksResult']
   }
 </script>
 <template>
-  <div>
-      <ul>
-        <li v-for="book in booksResult.docs" :key="book.author_key">{{book.title}}</li>
+<div>
+      <ul class="text-center mt-8 max-w-screen-md m-auto gap-4 grid sm:grid-cols-2 md:grid-cols-3">
+        <li class="font-bold text-slate-400 block p-6 max-w-full md:max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" v-for="book in booksResult.docs" :key="book.author_key">{{book.title}}</li>
       </ul>
-  </div>
+</div>
 </template>
 <style>
 </style>
