9f4553eb8ef902c57d8385f9110316c3b457dad5
Sergio <sergioa.orozcoo@gmail.com>
2022-08-22 23:09:43 -0500

    Add images to the render result

--- src/components/BookResult.vue
+++ src/components/BookResult.vue
@@ -2,17 +2,26 @@
   export default {
     data(){
       return{
-        booksLoading: false
+        booksLoading: false,
       }
     },
     props: ['booksResult']
   }
 </script>
 <template>
-<div>
-      <ul class="text-center mt-8 max-w-screen-md m-auto gap-4 grid sm:grid-cols-2 md:grid-cols-3">
-        <li class="font-bold text-slate-400 block p-6 max-w-full md:max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" v-for="book in booksResult.docs" :key="book.author_key">{{book.title}}</li>
-      </ul>
+<div class="text-center mt-8 max-w-screen-md m-auto gap-4 grid md:grid-cols-3">
+
+  <div v-for="book in booksResult.docs" :key="book.author_key" class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+          <img class="rounded-t-lg" :src="`https://covers.openlibrary.org/b/olid/${book.edition_key}-L.jpg`" alt="" />
+      </a>
+      <div class="p-5">
+          <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{book.title}}</h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{book.subtitle}}</p>
+      </div>
+  </div>
 </div>
 </template>
 <style>
--- src/components/InputAjax.vue
+++ src/components/InputAjax.vue
@@ -15,7 +15,7 @@ export default {
             e.preventDefault();
             const inputValue = document.getElementById("book-search").value;
             try {
-              await fetch(`${baseUrl}?q=${inputValue}`)
+              await fetch(`${baseUrl}?q=${inputValue}?limit=10`)
                   .then(res => res.json())
                   .then(data => this.vueLibrary = data)
             } catch(err) {
